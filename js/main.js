24fbb8f34112443e008158d1935aed86ab907eff
guili-guili <nft.theslurs@gmail.com>
2025-07-24 14:46:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+import { db } from "./firebase-config.js";
 
+import { ref, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+// Exemple : changer un statut
+function toggleOpenStatus(id, isOpen) {
+  set(ref(db, 'places/' + id + '/status'), isOpen);
+}
